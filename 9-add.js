a497585e354db1e735b48ca25197e9e44d6e5f49
Qortana <panashemoyo10@gmail.com>
2025-11-09 10:28:23 +0200

    adds a scriot that adds two integers

--- 9-add.js
+++ 9-add.js
@@ -1,8 +1,8 @@
-const a = parseInt(process.argv[2], 10);
-const b = parseInt(process.argv[3], 10);
-
-function add (x'y) {
-  return x + y;
+function add(a, b){
+  return a + b;
 }
 
-console.log(add(a,b));
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+console.log(add(a, b));
